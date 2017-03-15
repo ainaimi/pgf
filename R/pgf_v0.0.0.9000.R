8dcb08e5d0ff6e92fd31e732d32c34f5a18004ca
ainaimi <ashley.naimi@pitt.edu>
2017-03-15 13:13:51 -0400

    Added option for censoring adjustment or natural course. Corrected minor typos in program.

--- R/pgf_v0.0.0.9000.R
+++ R/pgf_v0.0.0.9000.R
@@ -14,18 +14,37 @@
 #' pgf()
 pgf<-function(ii,mc_data,length,randomization=NULL,exposure=NULL,censoring=NULL){
   # select first observation from baseline data
-  Vp<-mc_data[ii,]
-  # assign randomization indicator
-  Rp<-Vp$R
+  d<-mc_data[ii,]
   # length of follow-up
   lngth<-length
+  # first time-point is sampled empirical data
   # initiate variables to be simulated
-  Xp<-Bp<-Np<-Zp<-Cp<-Sp<-Dp<-Yp<-mm<-numeric()
-  Xp[1]<-Vp$X;Bp[1]<-Vp$B;Np[1]<-Vp$N;Zp[1]<-Vp$Z;
-  Cp[1]<-Vp$C;Sp[1]<-Vp$S;Dp[1]<-Vp$D;Yp[1]<-Vp$Y;
-  mm[1]<-j<-1;Vp<-Vp[,names(Vp) %in% c("id","V1","V2","V3","V4","V5","V6","V7","V8")]
-  id<-Vp$id;Vp$id<-NULL
-  # first time-point is sampled empirical data. for second time-point onwards, simulate
+  Vp<-Rp<-Xp<-Bp<-Np<-Zp<-Cp<-Sp<-Dp<-Yp<-mm<-numeric()
+  # time & id
+  mm[1]<-j<-1;id<-d$id;
+  # baseline & randomization indicator
+  Vp<-d[,names(d) %in% c("id","V1","V2","V3","V4","V5","V6","V7","V8")];Rp<-d$R
+  # time-varying covariates
+  Xp[1]<-d$X;Bp[1]<-d$B;Np[1]<-d$N;Zp[1]<-d$Z;
+  # outcomes
+  if(censoring==NULL){
+    Cp[1]<-0
+  } else if(censoring=="Natural"){
+    Cp[1]<-d$C
+  }
+  if(Cp[1]==1){
+    break
+  } else{
+    # by design, efuwp only possible after ~4 months
+    Sp[1]<-0
+    # fetal loss possible in month 1, therefore predict
+    Dp[1]<-ifelse(Zp[1]==1,
+                  pFunc(fitD,dDp),
+                  0)
+    # by design, live birth only possible after ~6 months
+    Yp[1]<-0
+  }
+  # second month onwards
   for(j in 2:lngth){
     # if no terminal events, then simulate
     if(Cp[j-1]==0&Sp[j-1]==0&Dp[j-1]==0&Yp[j-1]==0) {
@@ -36,7 +55,7 @@ pgf<-function(ii,mc_data,length,randomization=NULL,exposure=NULL,censoring=NULL)
       dBp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],Bl=Bp[j-1],Nl=Np[j-1],j)
       Bp[j]<-pFunc(fitB,dBp)
       # time-varying confounder 2
-      dNp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Xp[j-1],Nl=Xp[j-1],j)
+      dNp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Bp[j-1],Nl=Np[j-1],j)
       Np[j]<-pFunc(fitN,dNp)
       # time-varying confounder 3
       dZp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Bp[j-1],N=Np[j],Nl=Np[j-1],j)
@@ -44,10 +63,14 @@ pgf<-function(ii,mc_data,length,randomization=NULL,exposure=NULL,censoring=NULL)
                     pFunc(fitZ,dZp),
                     1)
       # outcome 1
-      dCp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Bp[j-1],N=Np[j],Nl=Xp[j-1],Z=Zp[j],j)
-      Cp[j]<-0 #pFunc(fitC,dCp) set to no censoring by default
+      dCp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Bp[j-1],N=Np[j],Nl=Np[j-1],Z=Zp[j],j)
+      if(censoring==NULL){
+        Cp[j]<-0
+      } else if(censoring=="Natural"){
+        Cp[j]<-pFunc(fitC,dCp)
+      }
       # outcome 2
-      dSp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Xp[j-1],N=Np[j],Nl=Xp[j-1],j)
+      dSp<-data.frame(Vp,R=Rp,X=Xp[j],Xl=Xp[j-1],B=Bp[j],Bl=Xp[j-1],N=Np[j],Nl=Np[j-1],j)
       if(j<=9){
         Sp[j]<-ifelse(Zp[j]==0&Cp[j]==0&j>4,
                       pFunc(fitS,dSp),
