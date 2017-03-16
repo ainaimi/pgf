3502ffb44e6ad3b7f281bb3688240a2725c7254d
ainaimi <ashley.naimi@pitt.edu>
2017-03-16 15:19:03 -0400

    Added multicore proccessing option to superlearner function call.

--- R/SL_function.R
+++ R/SL_function.R
@@ -1,6 +1,9 @@
 #' A SuperLearner Function
 #'
 #' This function allows you to fit the SuperLearner.
+#' @param multicore A binary integer (0,1) variable to choose parallel
+#' or sequential processing. If "0", standard SuperLearner is
+#' used. If "1" mcSupearLearner is used. Defaults to "0".
 #' @param dependent A numeric dependent variable, binomial or gaussian.
 #' @param independent A matrix with all independent variables.
 #' @param id A numeric individual identifier.
@@ -9,9 +12,15 @@
 #' @export
 #' @examples
 #' SL.model()
-SL.model <- function(dependent,independent,id,library,cores) {
-  output <- SuperLearner::SuperLearner(Y=dependent, X=independent,id=id,
-                                       SL.library=SL.library,
-                                       method="method.NNLS",family=binomial())
+SL.model <- function(multicore=0,dependent,independent,id,library) {
+  if(multicore==0){
+    output <- SuperLearner::SuperLearner(Y=dependent, X=independent,id=id,
+                                         SL.library=SL.library,
+                                         method="method.NNLS",family=binomial())
+  } else{
+    output <- SuperLearner::mcSuperLearner(Y=dependent, X=independent,id=id,
+                                             SL.library=SL.library,
+                                             method="method.NNLS",family=binomial())
+  }
   return(output)
 }
