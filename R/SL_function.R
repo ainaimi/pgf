2cbde1ec438ad030f6ea3587c44d5f07effa7ad0
ainaimi <ashley.naimi@pitt.edu>
2017-03-16 20:53:27 -0400

    Added snow functionality to SL.model for parallel processing.

--- R/SL_function.R
+++ R/SL_function.R
@@ -3,7 +3,8 @@
 #' This function allows you to fit the SuperLearner.
 #' @param multicore A binary integer (0,1) variable to choose parallel
 #' or sequential processing. If "0", standard SuperLearner is
-#' used. If "1" mcSupearLearner is used. Defaults to "0".
+#' used. If "1" mcSuperLearner is used. If "2" snowSuperLearner is used,
+#' and other arguments are required. Defaults to "0".
 #' @param dependent A numeric dependent variable, binomial or gaussian.
 #' @param independent A matrix with all independent variables.
 #' @param id A numeric individual identifier.
@@ -12,15 +13,22 @@
 #' @export
 #' @examples
 #' SL.model()
-SL.model <- function(multicore=0,dependent,independent,id,library) {
+SL.model <- function(multicore=0,dependent,independent,id,library,cores=4) {
   if(multicore==0){
     output <- SuperLearner::SuperLearner(Y=dependent, X=independent,id=id,
                                          SL.library=SL.library,
                                          method="method.NNLS",family=binomial())
-  } else{
+  } else if(multicore==1){
     output <- SuperLearner::mcSuperLearner(Y=dependent, X=independent,id=id,
                                              SL.library=SL.library,
                                              method="method.NNLS",family=binomial())
+  } else if(multicore==2){
+    cl<-makeCluster(cores, type="FORK")
+    clusterSetRNGStream(cl, iseed=123)
+    output <- SuperLearner::snowSuperLearner(cluster=cl,Y=dependent, X=independent,id=id,
+                                             SL.library=SL.library,
+                                             method="method.NNLS",family=binomial())
+    stopCluster(cl)
   }
   return(output)
 }
